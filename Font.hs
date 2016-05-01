aa5567d8b3a950b57fb0e04b81671ae56b694d2a
Daniel Lovasko <lovasko@freebsd.org>
2016-05-02 01:05:35 +0200

    changed Letter to Glyph

--- Font.hs
+++ Font.hs
@@ -1,8 +1,8 @@
 module Font
 ( Font(..)
-, Letter(..)
+, Glyph(..)
 , fontLoad
-, fontGetLetter
+, fontGetGlyph
 ) where
 
 import qualified Data.Map as MP
@@ -12,62 +12,62 @@ import Data.Bits (testBit)
 import Data.Char (ord, chr)
 import Data.List (intersperse)
 
-data Letter = Letter Int BS.ByteString
-type Font = MP.Map Char Letter
+data Glyph = Glyph Int BS.ByteString
+type Font = MP.Map Char Glyph 
 
--- ASCII art representation of the letter.
-instance Show Letter where
-	show (Letter w bytes) = concat $ intersperse "\n" (map row2string rows)
-		where
-			-- Split the letter into rows.
-			rows :: [BS.ByteString]
-			rows = splitEvery w bytes 
+-- ASCII art representation of the glyph.
+instance Show Glyph where
+  show (Glyph w bytes) = concat $ intersperse "\n" (map row2string rows)
+    where
+      -- Split the glyph into rows.
+      rows :: [BS.ByteString]
+      rows = splitEvery w bytes 
 
-			-- Convert the row into ASCII art.
-			row2string :: BS.ByteString -> String
-			row2string row = concatMap byte2string (BS.unpack row)
+      -- Convert the row into ASCII art.
+      row2string :: BS.ByteString -> String
+      row2string row = concatMap byte2string (BS.unpack row)
 
-			-- Convert a byte into eight ASCII characters based on zeros and ones.
-			byte2string :: Word8 -> String
-			byte2string byte = concatMap (bool2string . testBit byte) [7,6..0]
+      -- Convert a byte into eight ASCII characters based on zeros and ones.
+      byte2string :: Word8 -> String
+      byte2string byte = concatMap (bool2string . testBit byte) [7,6..0]
 
-			-- Define the ASCII art letters.
-			bool2string :: Bool -> String
-			bool2string True = "#"
-			bool2string False = "."
+      -- Define the ASCII art glpyhs.
+      bool2string :: Bool -> String
+      bool2string True = "#"
+      bool2string False = "."
 
 -- Split the ByteString into a list of smaller ByteStrings.
 splitEvery :: Int             -- size of each new sub-ByteString
            -> BS.ByteString   -- ByteString to split
-					 -> [BS.ByteString] -- list of sub-ByteStrings
+           -> [BS.ByteString] -- list of sub-ByteStrings
 splitEvery n bs
-	| n <= 0 || BS.null bs = []
-	| otherwise = BS.take n bs:splitEvery n (BS.drop n bs)
+  | n <= 0 || BS.null bs = []
+  | otherwise = BS.take n bs:splitEvery n (BS.drop n bs)
 
 -- Load an alphabet from the ByteString data.
 loadAlphabet :: Char             -- starting letter of the alphabet
-             -> Int              -- width of each letter in bytes
+             -> Int              -- width of each glyph in bytes
              -> [BS.ByteString]  -- input ByteString blocks
-             -> [(Char, Letter)] -- associative list of alphabet letters
-loadAlphabet start w bss = zipWith (,) [start .. end] letters
-	where
-		end = chr $ (ord start) + 25
-		letters = map (Letter w) (take 26 $ drop (ord start) bss)
+             -> [(Char, Glyph)] -- associative list of alphabet letters
+loadAlphabet start w bss = zipWith (,) [start .. end] glyphs 
+  where
+    end = chr $ (ord start) + 25
+    glyphs = map (Glyph w) (take 26 $ drop (ord start) bss)
 
 -- Load a FreeBSD bitmap font.
 fontLoad :: FilePath   -- path to the font file
-         -> (Int, Int) -- width and height of each letter in bits
+         -> (Int, Int) -- width and height of each glyph in bits
          -> IO Font    -- loaded font
 fontLoad path (w, h) = do
-	content <- BS.readFile path
-	let letters = splitEvery (w * h `div` 8) content
-	let lowerAlphabet = loadAlphabet 'a' (w `div` 8) letters
-	let upperAlphabet = loadAlphabet 'A' (w `div` 8) letters 
-	return $ MP.fromList (upperAlphabet ++ lowerAlphabet)
+  content <- BS.readFile path
+  let glyphs = splitEvery (w * h `div` 8) content
+  let lowerAlphabet = loadAlphabet 'a' (w `div` 8) glyphs 
+  let upperAlphabet = loadAlphabet 'A' (w `div` 8) glyphs 
+  return $ MP.fromList (upperAlphabet ++ lowerAlphabet)
 
--- Find a corresponding font letter for the character.
-fontGetLetter :: Font         -- font
-              -> Char         -- requested character
-              -> Maybe Letter -- a letter after the lookup
-fontGetLetter font char = MP.lookup char font
+-- Find a corresponding font glyph for the character.
+fontGetGlyph :: Font         -- font
+              -> Char        -- requested character
+              -> Maybe Glyph -- a glyph after the lookup
+fontGetGlyph font char = MP.lookup char font
 
