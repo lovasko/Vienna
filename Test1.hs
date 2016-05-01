cdd4fafecc46584990f41be4ba0e68550bc24320
Daniel Lovasko <lovasko@freebsd.org>
2016-05-02 00:06:25 +0200

    improved Test1 readability

--- Test1.hs
+++ Test1.hs
@@ -3,19 +3,25 @@ import Font
 import Data.Maybe (fromJust)
 import Data.BitString
 
-test1 :: Int -> Int -> IO () 
-test1 neuronCount limit = do
-	font <- fontLoad "swiss-8x16.fnt" (8, 16)
-	let (Letter _ letterA) = fromJust $ fontGetLetter font 'A'
-	let (Letter _ letterB) = fromJust $ fontGetLetter font 'B'
-	let (Letter _ letterZ) = fromJust $ fontGetLetter font 'Z'
-	let (Letter _ letterD) = fromJust $ fontGetLetter font 'D'
+-- By confusing the brain with two similar inputs, we
+-- produce a certain mixture of the output letters.
+test1 :: Int
+      -> Int
+      -> Int
+      -> IO () 
+test1 neuronCount teachLimit askLimit = do
+  font <- fontLoad "swiss-8x16.fnt" (8, 16)
+  let (Letter _ letter1) = fromJust $ fontGetLetter font 'i'
+  let (Letter _ letter2) = fromJust $ fontGetLetter font 'B'
+  let (Letter _ letter3) = fromJust $ fontGetLetter font 't'
+  let (Letter _ letter4) = fromJust $ fontGetLetter font 'G'
+  let (Letter _ letter5) = fromJust $ fontGetLetter font 'I'
 
-	b1 <- brainNew 128 neuronCount
-	let b2 = brainTeach b1 (bitString letterA, bitString letterB) limit
-	let b3 = brainTeach b2 (bitString letterB, bitString letterZ) limit 
+  b1 <- brainNew 128 neuronCount
+  let b2 = brainTeach b1 (bitString letter1, bitString letter2) teachLimit
+  let b3 = brainTeach b2 (bitString letter3, bitString letter4) teachLimit 
 
-	let output = brainAsk b3 (bitString letterD) limit
-	putStrLn $ show $ Letter 1 (realizeBitStringStrict output)
-	return ()
+  let output = brainAsk b3 (bitString letter5) askLimit
+  putStrLn $ show $ Letter 1 (realizeBitStringStrict output)
+  return ()
 
